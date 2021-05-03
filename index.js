1171c7630ddbb2e3a139d6e7534f135715f50b65
havidtech <76119744+havidtech@users.noreply.github.com>
2021-05-03 23:05:21 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,114 @@
+/*
+* Primary file for the API
+*
+*/
+
+// Dependencies
+const http = require('http');
+const https = require('https');
+const StringDecoder = require('string_decoder').StringDecoder;
+const config = require('./lib/config');
+const fs = require('fs');
+const handlers = require('./lib/handlers');
+const helpers = require('./lib/helpers');
+
+// instantiate http server
+var httpServer = http.createServer((req,res) => {
+    unifiedServer(req,res);
+
+});
+
+// Start the http server
+httpServer.listen(config.httpPort, () => {
+    console.log(`The server is listening on port ${config.httpPort}`);
+})
+
+// instantiate the HTTPS server
+var httpsServerOptions = {
+    'key': fs.readFileSync('./https/key.pem'),
+    'cert': fs.readFileSync('./https/cert.pem')
+};
+
+var httpsServer = http.createServer(httpsServerOptions,(req,res) => {
+    unifiedServer(req,res);
+
+});
+
+// Start the HTTPS server
+httpsServer.listen(config.httpsPort, () => {
+    console.log(`The server is listening on port ${config.httpsPort}`);
+})
+
+// All the server logic for both the http and https server
+var unifiedServer = function(req,res){
+
+    // Get the URL and parse it 
+    var parsedUrl = new URL(req.url, `http://${req.headers.host}`);
+
+    // Get the path
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    // Get the query string as an object
+    var queryStringObject = parsedUrl.searchParams;
+
+    // Get the HTTP Method
+    var method = req.method.toLowerCase();
+
+    // Get the headers as an object
+    var headers = req.headers;
+
+    // Get the payload, if any
+    var decoder = new StringDecoder('utf-8');
+    var buffer = '';
+    req.on('data', (data) => {
+        buffer += decoder.write(data);
+    });
+
+    req.on('end', () => {
+        buffer += decoder.end();
+
+        // Choose the handler that this request should go to. If one is not found use the notFound handler
+        var choosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath]: handlers.notFound;
+        
+        // Construct the data object to send to the handler
+        var data = {
+            'trimmedPath' : trimmedPath,
+            'queryStringObject': queryStringObject,
+            'method': method,
+            'headers': headers,
+            'payload': helpers.parseJsonToObject(buffer)
+        };
+
+        // route the request to the handler specified in the router
+        choosenHandler(data, (statusCode, payload)=>{
+            // Use the status code called back by the handler, or default to 200
+            statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+
+            // Use the payload called back by the handler, or default to an empty object
+            payload = typeof(payload) == 'object' ? payload: {};
+
+            // Convert the payload to a string
+            var payloadString = JSON.stringify(payload);
+
+            // Return the response
+            res.setHeader('Content-Type', 'application/json');
+            res.writeHead(statusCode);
+            res.end(payloadString);
+
+            // Log the request path
+             console.log(`Returning this response: ${statusCode} ${payload}`);
+
+        });
+    });
+
+};
+
+// Define a request router
+var router = {
+    'ping': handlers.ping,
+    'users': handlers.users,
+    'sampleUpdate': handlers.sampleUpdate,
+    'sampleWrite': handlers.sampleWrite
+
+};
